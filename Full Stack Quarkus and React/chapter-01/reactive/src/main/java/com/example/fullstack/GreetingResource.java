2c9e94f4f99debf18c7e060cf134514be31f4e37
João Pedro Nolasco <nolascojoaops@gmail.com>
2024-04-19 14:38:03 -0300

    CH-01: Add first Quarkus project structure

---
+++ Full Stack Quarkus and React/chapter-01/reactive/src/main/java/com/example/fullstack/GreetingResource.java
@@ -0,0 +1,23 @@
+package com.example.fullstack;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+@Path("/hello")
+public class GreetingResource {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        return "Hello Quarkus live coding!";
+    }
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    @Path("/world")
+    public String helloWorld() {
+        return "Hello world";
+    }
+}
