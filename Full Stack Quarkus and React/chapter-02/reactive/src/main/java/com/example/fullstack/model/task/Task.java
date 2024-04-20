c08b4b1396a68a3282e84dd5985c4563a1a2306e
João Pedro Nolasco <nolascojoaops@gmail.com>
2024-04-20 19:17:50 -0300

    CH-02: Add task entity for users to define and track tasks

---
+++ Full Stack Quarkus and React/chapter-02/reactive/src/main/java/com/example/fullstack/model/task/Task.java
@@ -0,0 +1,38 @@
+package com.example.fullstack.model.task;
+
+import com.example.fullstack.model.project.Project;
+import com.example.fullstack.model.user.User;
+import io.quarkus.hibernate.reactive.panache.PanacheEntity;
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.ZonedDateTime;
+
+@Entity
+@Table(name = "tasks")
+public class Task extends PanacheEntity {
+
+    @Column(nullable = false)
+    public String title;
+
+    @Column(length = 1000)
+    public String description;
+
+    public Integer priority;
+
+    @ManyToOne(optional = false)
+    public User user;
+
+    public ZonedDateTime complete;
+
+    @ManyToOne
+    public Project project;
+
+    @CreationTimestamp
+    @Column(updatable = false, nullable = false)
+    public ZonedDateTime created;
+
+    @Version
+    public int version;
+
+}
