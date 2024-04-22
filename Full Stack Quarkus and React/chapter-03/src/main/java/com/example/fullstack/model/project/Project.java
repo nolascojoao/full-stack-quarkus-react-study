6aa6d02c6c4ecbd39e7ad625e5d7c98ba2df5986
João Pedro Nolasco <nolascojoaops@gmail.com>
2024-04-22 20:38:46 -0300

    CH-03: Chore: Clone chapter2 folder for further work

---
+++ Full Stack Quarkus and React/chapter-03/src/main/java/com/example/fullstack/model/project/Project.java
@@ -0,0 +1,32 @@
+package com.example.fullstack.model.project;
+
+import com.example.fullstack.model.user.User;
+import io.quarkus.hibernate.reactive.panache.PanacheEntity;
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.ZonedDateTime;
+
+@Entity
+@Table(
+        name = "projects",
+        uniqueConstraints = {
+                @UniqueConstraint(columnNames = {"name", "user_id"})
+        }
+)
+public class Project extends PanacheEntity {
+
+    @Column(nullable = false)
+    public String name;
+
+    @ManyToOne(optional = false)
+    public User user;
+
+    @CreationTimestamp
+    @Column(updatable = false, nullable = false)
+    public ZonedDateTime created;
+
+    @Version
+    public int version;
+
+}
